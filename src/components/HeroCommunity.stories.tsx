23aae3477a32c07406653bf646a7849ea19b78c3
Ismael Martinez Ramos <ismael.martinez@postcodelottery.co.uk>
2025-08-03 22:52:18 +0100

    Feat: Add Storybook stories for HeroCommunity component

---
+++ src/components/HeroCommunity.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HeroCommunity from './HeroCommunity';
+
+const meta: Meta<typeof HeroCommunity> = {
+  title: 'Components/HeroCommunity',
+  component: HeroCommunity,
+  parameters: {
+    layout: 'fullscreen',
+    clerk: { enabled: false }, // This component does not use Clerk directly
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    // HeroCommunity does not have any direct props
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof HeroCommunity>;
+
+export const Default: Story = {
+  args: {},
+};
